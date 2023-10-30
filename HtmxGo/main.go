036cef820cb690348d3f00c640453f501933c583
Christoph Suessmeier <mail@suessmeier.net>
2023-10-30 14:56:17 +0100

    Added simple SQLite stuff... commit before splitting into packages...

--- HtmxGo/main.go
+++ HtmxGo/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 	"os"
@@ -113,8 +115,99 @@ func getHtmlTest(w http.ResponseWriter, r *http.Request) {
 	html.Execute(w, "This has been added by the API function")
 }
 
+// For SQL rows
+
+type TestRow struct {
+	id          int
+	name        string
+	description string
+	active      bool
+}
+
 func main() {
 	port := fmt.Sprintf(":%s", getEnv("PORT", "3000"))
+
+	// prepare sqlite connection
+	db, err := sql.Open("sqlite3", "./db/todo.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var (
+		id          int
+		name        string
+		description string
+		active      bool
+	)
+
+	// apparently it's better to prepare statements in order to reduce the total number of
+	// calls made against the db
+	// HOWEVER, this is only really true if a statement is reused multiple times 
+	// (e.g. regular re-accessing of data)
+	statement, err := db.Prepare("SELECT * FROM todos WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer statement.Close()
+
+
+	// shorthand for querying a single row
+	err = statement.QueryRow(2).Scan(&id, &name, &description, &active)
+	if err != nil {
+		// Best way to handle errors for queries is check whether any rows have been returned
+		// at all
+		if err == sql.ErrNoRows {
+			log.Println("No rows returned :(")
+		} else {
+			log.Fatal(err)
+		}
+	}
+	log.Printf("Result: %d\t%s\t%s\t%v", id, name, description, active)
+
+	// // let's add a new test row
+	// stmt, err := db.Prepare("INSERT INTO todos(name, description, active) VALUES ($1, $2, $3)")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// // we can use _ if we don't care for the statement result
+	// _, err = stmt.Exec("Insert Test", "Let's thest creating a row via Go", 1)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer stmt.Close()
+
+
+	// ALL OF THE BELOW IS HOW TO QUERY MULTIPLE ROWS
+	// run a test query
+	// rows, err := db.Query("select * from todos where id = ?", 1)
+	// if err != nil {
+	// 	// just for testing, failed requests should not kill the server
+	// 	log.Fatal(err)
+	// }
+	// defer rows.Close()
+	//
+	// for rows.Next() {
+	// 	var (
+	// 		id          int
+	// 		name        string
+	// 		description string
+	// 		active      bool
+	// 	)
+	//
+	// 	err := rows.Scan(&id, &name, &description, &active)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	//
+	// 	// TODO: Here we would create a new TestRow and move the values in probably?
+	// 	log.Printf("Result: %d\t%s\t%s\t%v", id, name, description, active)
+	// }
+	// // we need to check if something happend in the loopdiloop
+	// err = rows.Err()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	// new server
 	mux := http.NewServeMux()
@@ -131,7 +224,7 @@ func main() {
 	mux.HandleFunc("/api/htmx", getHtmlTest)
 
 	// run server
-	err := http.ListenAndServe(port, mux)
+	err = http.ListenAndServe(port, mux)
 
 	if err == nil {
 		log.Fatal(err)
